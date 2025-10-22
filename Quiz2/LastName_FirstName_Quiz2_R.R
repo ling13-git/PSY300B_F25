fcfc10d6af0f28f6d9aa2420c5615aecf54d0a29
ling13-git <ling1@davidson.edu>
2025-10-22 15:26:59 -0400

    Add files via upload

---
+++ Quiz2/LastName_FirstName_Quiz2_R.R
@@ -0,0 +1,14 @@
+#reminder: remember to activate your packages
+#note: write up the APA results in the corresponding word document
+
+####QUESTION 1####
+#[Enter your code for Question 1. You can delete this line after your response is entered.]
+
+
+####QUESTION 2####
+#[Enter your code for Question 2. You can delete this line after your response is entered.]
+
+
+####QUESTION 3####
+#[Enter your code for Question 3. You can delete this line after your response is entered.]
+
